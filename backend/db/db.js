c5ff08e1652e1f0f5f2034d243e69407613c0b44
Suresh Alabani <94789102+SURESH2218@users.noreply.github.com>
2024-11-13 16:37:18 +0530

    no need of vite

--- backend/db/db.js
+++ backend/db/db.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(`${process.env.VITE_MONGODB_URI}`);
+    await mongoose.connect(`${process.env.MONGODB_URI}`);
     console.log("mongodb connected");
   } catch (error) {
     console.log("MONGOODB CONNECTION ERROR", error);
