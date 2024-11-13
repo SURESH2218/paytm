3e4e744fee8091dd2d6ee1ff02f4e45309ee6f93
Suresh Alabani <94789102+SURESH2218@users.noreply.github.com>
2024-11-13 20:38:59 +0530

    replaced this file with userRouter

--- backend/routes/index.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { signUpUser, singinUser } from "../controllers/UserController.js";
-
-const router = express.Router();
-
-router.post("/signup", signUpUser);
-router.post("/signin", singinUser);
-
-export default router;
