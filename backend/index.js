8da6ab15ed2b8d0db3c77d9eb91132271cbaf5c6
Suresh Alabani <94789102+SURESH2218@users.noreply.github.com>
2024-11-13 16:40:01 +0530

    added cors to accept the inputs from the frontend origin

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,17 @@
 import connectDB from "./db/db.js";
-
 import express from "express";
-import mainRouter from "./routes/index.js";
+import cors from "cors";
 const app = express();
 app.use(express.json());
+app.use(
+  cors({
+    origin: "process.env.CORS_ORIGIN",
+    credentials: true,
+  })
+);
 connectDB();
 
-app.use("api/v1", mainRouter);
+import mainRouter from "./routes/index.js";
+app.use("/api/v1", mainRouter);
+
 app.listen(3000);
