de725bad15f4cebe0f2907479db0225780995f3b
Suresh Alabani <94789102+SURESH2218@users.noreply.github.com>
2024-11-13 20:38:30 +0530

    created userRouter

---
+++ backend/routes/userRouter.js
@@ -0,0 +1,17 @@
+import express from "express";
+import {
+  filterUser,
+  signUpUser,
+  singinUser,
+  updatebody,
+} from "../controllers/UserController.js";
+import authMiddleware from "../middlewares/auth.middleware.js";
+
+const router = express.Router();
+
+router.post("/user/signup", signUpUser);
+router.post("/user/signin", singinUser);
+router.put("/user/update", authMiddleware, updatebody);
+router.get("/users/filterUser", filterUser);
+
+export default router;
