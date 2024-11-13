92a4b7e238c97131c45daee0f83306f87caafbc7
Suresh Alabani <94789102+SURESH2218@users.noreply.github.com>
2024-11-13 13:34:23 +0530

    initial commit

---
+++ Paytm/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="">
+      <h1 className="text-4xl bg-black px-2 py-1 text-white inline-block rounded-md">
+        suresh
+      </h1>
+    </div>
+  );
+}
+
+export default App;
